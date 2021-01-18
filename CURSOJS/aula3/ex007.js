29ddd16296c59d835d5426e3f3bdb149f24b8231
Daniel <69875036+Danieloliver11@users.noreply.github.com>
2021-01-18 16:14:24 -0300

    .

---
+++ CURSOJS/aula3/ex007.js
@@ -0,0 +1,16 @@
+//Condição simples em JavaScript
+var a = 40
+if (a > 40) {
+    console.log(`Você passou da velocidade permitida de 40km!`)
+}
+console.log("Use cinto de segurança!")
+
+
+//Condição composta
+console.log("Em que pais você esta?")
+var b = "eua"
+if (b != "Brasil") {
+    console.log(" você é Estrangeiro!")
+} else {
+    console.log("você é Brasileiro")
+}
