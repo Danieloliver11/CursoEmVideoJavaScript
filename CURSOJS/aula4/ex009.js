8a4f75da18b81f03378bf4256e26c08297efdc26
Daniel <69875036+Danieloliver11@users.noreply.github.com>
2021-01-19 15:43:07 -0300

    .

--- CURSOJS/aula4/ex009.js
+++ CURSOJS/aula4/ex009.js
@@ -1,8 +1,22 @@
 //laço de repetição
-var n = 1
+//estrutura de repetição com teste logico no inicio!
+var n = 0
+var m = 0
 
-while (nu <= 100) {
+while (n < 10) {
     n = n + 1
-    console.log(n)
+    console.log(n + ' com while!')
 }
-console.log(n + 'resultado')
+
+//estrutura de repetição com teste logico no final! do (faça)
+
+do {
+    m = m + 1
+    console.log(m + ' com DOOOOOO while!')
+}
+while (m < 10)
+
+// FOR; for(INICIALISAÇÃO ; TESTE LOGICO ; INGREMENTO)
+for (var f = 0; f <= 10; f = f + 1) {
+    console.log(f + ' com FOR !!!')
+}
