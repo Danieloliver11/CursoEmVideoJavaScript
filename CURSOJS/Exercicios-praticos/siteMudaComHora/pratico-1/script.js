7a5a251166902545bb71e30fb6fa243193503857
Daniel <69875036+Danieloliver11@users.noreply.github.com>
2021-01-27 22:01:57 -0300

    .

--- CURSOJS/Exercicios-praticos/siteMudaComHora/pratico-1/script.js
+++ CURSOJS/Exercicios-praticos/siteMudaComHora/pratico-1/script.js
@@ -3,21 +3,26 @@ function contar() {
     var fim = Number(document.getElementById('fim').value)
     var passo = Number(document.getElementById('passo').value)
     var res = document.getElementById('saida')
-    var resul
-    inicio = 0
-    res.innerHTML = "Contando: "
-        //verificação
+    var pri = inicio
+    var emoti = "&#128073"
+    var bandeira = "&#127937"
+
+    //verificação
     if (inicio === 0 && fim === 0) {
         alert('[ERRO] Inicio e fim sem valor!')
-    } else if (passo == 0) {
+    } else
+    if (passo === 0) {
         passo = 1
-    }
 
+    }
+    res.innerText = "Contando:"
+    res.innerHTML += `${emoti} ${pri}`
 
     while (inicio < fim) {
-        resul = inicio + passo
-        res.innerHTML = resul
+
+        inicio = inicio + passo
+        res.innerHTML += ` ${emoti} ${inicio} `
     }
 
-
+    res.innerHTML += bandeira
 }
